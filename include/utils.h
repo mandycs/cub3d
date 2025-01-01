a1461315f0401545733362130f84deb5aaeb498e
SrVariable <rojohnibana@gmail.com>
2025-01-01 12:16:48 +0100

    Define some default colors

--- include/utils.h
+++ include/utils.h
@@ -1,5 +1,25 @@
+/**
+ * TODO: Refactor utils.h into colors.h and vector.h
+ */
 #ifndef UTILS_H
 # define UTILS_H
+
+/* @------------------------------------------------------------------------@ */
+/* |                            Define Section                              | */
+/* @------------------------------------------------------------------------@ */
+
+#define WHITE ((t_color){.r = 255, .g = 255, .b = 255, .a = 255})
+#define BLACK ((t_color){.r = 0, .g = 0, .b = 0, .a = 255})
+#define RED ((t_color){.r = 255, .g = 0, .b = 0, .a = 255})
+#define GREEN ((t_color){.r = 0, .g = 255, .b = 0, .a = 255})
+#define BLUE ((t_color){.r = 0, .g = 0, .b = 255, .a = 255})
+#define YELLOW ((t_color){.r = 255, .g = 255, .b = 0, .a = 255})
+#define LIGHTRED ((t_color){.r = 255, .g = 127, .b = 127, .a = 255})
+#define LIGHTGREEN ((t_color){.r = 127, .g = 255, .b = 127, .a = 255})
+#define LIGHTBLUE ((t_color){.r = 127, .g = 127, .b = 255, .a = 255})
+#define LIGHTYELLOW ((t_color){.r = 255, .g = 255, .b = 127, .a = 255})
+#define DARKGRAY ((t_color){.hex = 0x303030FF})
+#define GRAY ((t_color){.hex = 0x606060FF})
 
 /* @------------------------------------------------------------------------@ */
 /* |                            Typedef Section                             | */
