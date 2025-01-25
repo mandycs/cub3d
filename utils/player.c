5ac17622031b8beda9b46c2790dce5a4127750d7
SrVariable <rojohnibana@gmail.com>
2025-01-25 15:29:55 +0100

    Change player angle depending on the char in map

--- utils/player.c
+++ utils/player.c
@@ -6,7 +6,7 @@
 /*   By: ribana-b <ribana-b@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 21:26:40 by ribana-b          #+#    #+# Malaga      */
-/*   Updated: 2025/01/25 15:17:38 by ribana-b         ###   ########.com      */
+/*   Updated: 2025/01/25 15:26:45 by ribana-b         ###   ########.com      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,14 @@ bool	create_player(t_player *player, t_cub *cub)
 		return (false);
 	player->speed = 10;
 	player->fov = 90;
-	player->angle = 0;
+	if (cub->map[cub->pos_y][cub->pos_x] == 'E')
+		player->angle = 0;
+	else if (cub->map[cub->pos_y][cub->pos_x] == 'S')
+		player->angle = 90;
+	else if (cub->map[cub->pos_y][cub->pos_x] == 'W')
+		player->angle = 180;
+	else if (cub->map[cub->pos_y][cub->pos_x] == 'N')
+		player->angle = 270;
 	log_info("Created player");
 	return (true);
 }
