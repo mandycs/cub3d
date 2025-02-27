0b77423e4a8c7e2d65487accdf00d0735ee0e401
SrVariable <rojohnibana@gmail.com>
2025-02-27 18:58:48 +0100

    Remove some old unused code from render

--- utils/render.c
+++ utils/render.c
@@ -6,84 +6,11 @@
 /*   By: ribana-b <ribana-b@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/18 21:26:43 by ribana-b          #+#    #+# Malaga      */
-/*   Updated: 2025/02/27 14:04:10 by ribana-b         ###   ########.com      */
+/*   Updated: 2025/02/27 18:57:30 by ribana-b         ###   ########.com      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "utils.h"
-
-// TODO?: When the player is outside of the map it doesn't render anything
-// *Maybe it is not necessary to see anything because I'll implement
-// wall collision later.
-void	render_view(t_player *player, t_map *map, t_screen *screen)
-{
-	double	step;
-	double	angle;
-	double	distance;
-	int		x;
-
-	angle = bfl_mod(player->angle - player->fov * 0.5, 360);
-	step = (double)player->fov / screen->width;
-	x = 0;
-	while (x < screen->width)
-	{
-		distance = calculate_distance(player, map, angle);
-		if (distance)
-			render_wall(screen, x, distance, lightgreen());
-		angle += step;
-		++x;
-	}
-}
-
-void	render_map(t_map *map, t_v2 *position, t_screen *screen)
-{
-	int	i;
-	int	j;
-
-	i = -1;
-	(void)position;
-	while (++i < map->rows)
-	{
-		j = -1;
-		while (++j < map->cols)
-		{
-			render_map_element(map, screen, i, j);
-		}
-	}
-}
-
-void	render_player(t_player *player, t_screen *screen)
-{
-	t_v2	position;
-	t_v2	size;
-
-	position = v2_create(player->position.y * screen->scale,
-			player->position.x * screen->scale);
-	size = v2_create(screen->scale, screen->scale);
-	draw_rectangle(screen->buffer, position, size, lightred());
-}
-
-void	render_fov(t_player *player, t_map *map, t_screen *screen)
-{
-	t_v2	start;
-	t_v2	end;
-	int		i;
-
-	start = v2_create(player->position.y * screen->scale + screen->scale * 0.5,
-			player->position.x * screen->scale + screen->scale * 0.5);
-	end = calculate_wall_collision2(start, player->angle * DEG2RAD, map);
-	draw_line(screen->buffer, start, end, green());
-	i = -1;
-	while (++i < player->fov * 0.5)
-	{
-		end = calculate_wall_collision2(start,
-				(player->angle + i) * DEG2RAD, map);
-		draw_line(screen->buffer, start, end, green());
-		end = calculate_wall_collision2(start,
-				(player->angle - i) * DEG2RAD, map);
-		draw_line(screen->buffer, start, end, green());
-	}
-}
 
 // NOTE: The player position will be taked into account to render the minimap,
 // so I can have a fixed size instead of drawing the whole map, useful for
@@ -179,7 +106,10 @@ void new_cast_ray(t_screen *screen, t_map *map, t_player *player, double rangle)
         {
             hit = true;
 			//draw_rectangle(screen->buffer, v2_create(res.y, res.x), v2_create(1, 1), yellow());
-			draw_line(screen->buffer, v2_create(player->position.y * size + size * 0.5, player->position.x * size + size * 0.5), v2_create(res.y, res.x), lightgreen());
+			draw_line(screen->buffer,
+					v2_create(player->position.y * size + size * 0.5, player->position.x * size + size * 0.5),
+					v2_create(res.y, res.x),
+					lightgreen());
         }
     }
 }
@@ -345,15 +275,10 @@ void	render(void *param)
 		new_render_view(&info->screen, &info->map, &info->player, newRangleView, x, info->img);
 	}
 	new_render_minimap(&info->screen, &info->map, &info->player);
-	for (int a = 0; a < info->player.fov * 0.5; ++a)
+	for (double a = 0; a < info->player.fov * 0.5; a += 0.5)
 	{
-		new_cast_ray(&info->screen, &info->map, &info->player, (info->player.angle + a) * DEG2RAD);
-		new_cast_ray(&info->screen, &info->map, &info->player, (info->player.angle - a) * DEG2RAD);
+		new_cast_ray(&info->screen, &info->map, &info->player, fmod(info->player.angle + a, 360) * DEG2RAD);
+		new_cast_ray(&info->screen, &info->map, &info->player, fmod(info->player.angle - a, 360) * DEG2RAD);
 	}
 	new_render_player(&info->screen, &info->player);
-
-	//render_view(&info->player, &info->map, &info->screen);
-	//render_map(&info->map, &info->player.position, &info->screen);
-	//render_player(&info->player, &info->screen);
-	//render_fov(&info->player, &info->map, &info->screen);
 }
